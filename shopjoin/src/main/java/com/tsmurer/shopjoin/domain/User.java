1dab2291868b16437095705dbcc6c3a6ade87dac
tiagoMurer <tiagostaurenghi@hotmail.com>
2021-03-08 17:50:41 -0300

    desabilitado spring security temporariamente

--- shopjoin/src/main/java/com/tsmurer/shopjoin/domain/User.java
+++ shopjoin/src/main/java/com/tsmurer/shopjoin/domain/User.java
@@ -10,7 +10,8 @@ import java.util.Set;
 @Table(name = "user")
 public class User {
 
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name = "user_id")
     private Long userId;
     @Column(name = "username")
